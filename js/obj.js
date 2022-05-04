541e928908848fe0b5cfd3664757d8ac27fbb950
harrypatiry <harrypatiry@gmail.com>
2022-05-04 16:28:58 -0400

    normal

--- js/obj.js
+++ js/obj.js
@@ -1,21 +1,24 @@
 const section = document.querySelector("section.obj")
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
 const renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setClearColor( 0x000000, 0 );
 renderer.setSize( window.innerWidth, window.innerHeight );
 section.appendChild( renderer.domElement );
 
-
-const loader = new THREE.TextureLoader();
+const textureLoader = new THREE.TextureLoader();
+const rockBase = textureLoader.load('assets/rock.png');
+const rockNormal = textureLoader.load('assets/rocknormal.png');
 
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshLambertMaterial( { map: loader.load('assets/rock.png') } );
+const material = new THREE.MeshStandardMaterial( { 
+    map: rockBase,
+    normalMap: rockNormal
+} );
 const cube = new THREE.Mesh( geometry, material );
 
 const light = new THREE.AmbientLight( 0x404040 );
-const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+const directionalLight = new THREE.DirectionalLight( 0xffffff, .9 );
 scene.add( cube, light, directionalLight );
 scene.background = null;
 
