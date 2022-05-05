c7356dd4b557f0726735fbf10f766e6525a081e3
harrypatiry <harrypatiry@gmail.com>
2022-05-05 13:43:36 -0400

    nav

--- js/index.js
+++ js/index.js
@@ -1,8 +1,7 @@
-// const container = document.querySelector('.container');
-// let scrollLine = document.querySelector('.scroll');
-
-// container.addEventListener('wheel', (e)=> {
-//     e.preventDefault();
-//     container.scrollLeft += e.deltaY;
-//     scrollLine.style.width = container.scrollLeft / 2 + 'px';
-// })
+window.onload = function() {
+    const menubtn = document.querySelector('.menu');
+    const links = document.querySelector('.links');
+    menubtn.addEventListener('click', function() {
+        links.classList.toggle('active');
+    })
+}
