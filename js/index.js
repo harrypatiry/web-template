03336e7d1cf79c73ff8880ff99db3b58e0c6fe3c
harrypatiry <harrypatiry@gmail.com>
2022-05-07 16:30:18 -0400

    img scroll

--- js/index.js
+++ js/index.js
@@ -6,13 +6,13 @@ window.onload = function() {
     })
     let imgs = document.getElementById('imgs');
     let imgstwo = document.getElementById('imgstwo');
-    let speed = 10;
+    let speed = 40;
     window.onscroll = function () {
         let yOffset = window.scrollY;
         imgs.style.top = (yOffset / speed) + "px";
-        imgs.style.right = ((yOffset / speed)/ 6) + "px";
-        imgstwo.style.top = ((yOffset / speed)/4) + "px";
-        imgstwo.style.left = ((yOffset / speed)/ 8) + "px";
+        imgs.style.right = (yOffset / speed) + "px";
+        imgstwo.style.top = (yOffset / speed) + "px";
+        imgstwo.style.left = (yOffset / speed) + "px";
     }
 }
 
