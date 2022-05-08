72b9aa41237e862bf20f63ec9f2f8d6fa4032428
harrypatiry <harrypatiry@gmail.com>
2022-05-08 10:58:11 -0400

    new nav

--- js/index.js
+++ js/index.js
@@ -1,9 +1,18 @@
 window.onload = function() {
-    const menubtn = document.querySelector('.menu');
-    const links = document.querySelector('.links');
-    menubtn.addEventListener('click', function() {
-        links.classList.toggle('active');
+    const mobile = document.querySelector('.mobile-menu');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    mobile.addEventListener('click', function() {
+        nav.classList.toggle('nav-active');
+        navLinks.forEach((link, index) => {
+            if(link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade .5s ease forwards ${index / 4}s`;
+            }
+        })
     })
+
     let imgs = document.getElementById('imgs');
     let imgstwo = document.getElementById('imgstwo');
     let speed = 40;
